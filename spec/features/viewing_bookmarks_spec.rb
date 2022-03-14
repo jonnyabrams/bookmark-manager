d16c1b61384764723e8c9ea570f273f63f2a4e28
PhilBligh <philbligh@hotmail.com>
2022-03-14 15:21:42 +0000

    setup bookmark class

--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -1,6 +1,16 @@
-feature 'Viewing bookmarks' do
+feature 'Homepage' do
   scenario 'visiting the index page' do
     visit('/')
     expect(page).to have_content "Bookmark Manager"
+  end
+end
+
+feature 'Viewing bookmarks' do
+  scenario 'visiting the bookmark page' do
+    visit('/bookmarks')
+    
+    expect(page).to have_content "http://www.makersacademy.com"
+    expect(page).to have_content "http://www.destroyallsoftware.com"
+    expect(page).to have_content "http://www.google.com"
   end
 end
