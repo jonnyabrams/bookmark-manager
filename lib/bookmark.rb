8027ef2bff1400b0baf8278fa66822642e57deb4
Jonny Abrams <jonnyabrams@hotmail.com>
2022-03-15 16:40:49 +0000

    Made database helper
    Co-authored-by: Phil Bligh <PhilBligh@users.noreply.github.com>

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -16,10 +16,10 @@ class Bookmark
     else
       connection = PG.connect(dbname: 'bookmark_manager')
     end
-
-   result = connection.exec("SELECT * FROM bookmarks;")
-   result.map { |bookmark| 
-   Bookmark.new(id: bookmark['id'], title: bookmark['title'], url: bookmark['url']) }
+    result = connection.exec("SELECT * FROM bookmarks")
+    result.map do |bookmark|
+      Bookmark.new(id: bookmark['id'], title: bookmark['title'], url: bookmark['url'])
+    end
   end
 
   def self.create(url:, title:)
@@ -28,7 +28,7 @@ class Bookmark
     else
       connection = PG.connect(dbname: 'bookmark_manager')
     end
-
-    connection.exec("INSERT INTO bookmarks (title, url) VALUES('#{title}', '#{url}') RETURNING id, url, title")
+    result = connection.exec("INSERT INTO bookmarks (url, title) VALUES('#{url}', '#{title}') RETURNING id, title, url;")
+    Bookmark.new(id: result[0]['id'], title: result[0]['title'], url: result[0]['url'])
   end
 end
