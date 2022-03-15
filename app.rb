a08785a20ecdc4f09cbf067faeaa352d8477eddf
Jonny Abrams <jonnyabrams@hotmail.com>
2022-03-15 15:28:56 +0000

    Add create to Bookmark and views
    Co-authored-by: Phil Bligh <PhilBligh@users.noreply.github.com>

--- app.rb
+++ app.rb
@@ -12,13 +12,18 @@ class BookmarkManager < Sinatra::Base
   end
 
   get '/bookmarks' do
-
     @bookmarks = Bookmark.all
     erb :'bookmarks/index'
-
   end
 
+  get '/bookmarks/new' do
+    erb :"bookmarks/new"
+  end
 
+  post '/bookmarks' do
+    Bookmark.create(url: params[:url])
+    redirect '/bookmarks'
+  end
 
   run! if app_file == $0
 end
