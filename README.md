a51080c5e252635cbdc8113fa6f6d4d528c28681
PhilBligh <philbligh@hotmail.com>
2022-03-15 14:37:13 +0000

    setting up testing environment

--- README.md
+++ README.md
@@ -9,3 +9,9 @@ I would like to be shown a list of bookmarks
 3. Connect to the database using the pqsl command \c bookmark_manager;
 
 4. Run the query we have saved in the file 01_create_bookmarks_table.sql
+
+5. Create a test database using the psql command CREATE DATABASE bookmark_manager_test;
+
+6. Connect to the test database using the pqsl command \c bookmark_manager_test;
+
+7. Run the query we have saved in the file 01_create_bookmarks_table.sql
--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -6,7 +6,13 @@ feature 'Homepage' do
 end
 
 feature 'Viewing bookmarks' do
-  scenario 'visiting the bookmark page' do
+  scenario 'A user can see bookmarks' do
+    connection = PG.connect(dbname: 'bookmark_manager_test')
+
+    connection.exec("INSERT INTO bookmarks VALUES(1, 'http://www.makersacademy.com');")
+    connection.exec("INSERT INTO bookmarks VALUES(2, 'http://www.destroyallsoftware.com');")
+    connection.exec("INSERT INTO bookmarks VALUES(3, 'http://www.google.com');")
+    
     visit('/bookmarks')
     
     expect(page).to have_content "http://www.makersacademy.com"
